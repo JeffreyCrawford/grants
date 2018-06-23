a15a747c1dbd4e89427015794b365d215ce79aa4
Jeff Crawford <jeffrey.a.crawford@gmail.com>
2018-06-23 11:51:04 -0400

    server and component setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap-theme.css';
 import './App.css';
 
 class App extends Component {
